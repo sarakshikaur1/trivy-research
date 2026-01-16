18a25635df343a0d8621f8eb6e8cc8560bca3bd7
Nishant Sharma <nishant@covalience.com>
2026-01-16 16:21:22 +0530

    updated trivy.yaml to test scan for dotnet

---
+++ docker/SampleDotNetApp/Program.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello from Sample .NET App!");
+
+app.Run();
