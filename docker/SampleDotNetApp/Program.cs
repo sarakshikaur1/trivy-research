7979a9dd666e0ab4d585ed55fa50ff86a3422d4c
Nishant Sharma <nishant@covalience.com>
2026-01-22 11:59:29 +0530

    reverting to old changes

--- docker/SampleDotNetApp/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello from Sample .NET App!");
-
-app.Run();
